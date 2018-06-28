7150f96c37b7da926e88fe7c6e43ba9d282731e6
mashudumelton <mashudumelon@gmail.com>
2018-06-28 08:35:31 -0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+valueone: number;
+valuetwo: number 
+
+  constructor(public navCtrl: NavController) {
+ this.valueone = 0;
+ this.valuetwo =0;
+  }
+onClick1(){
+  // console.log(this.value)
+  this.valueone +=3;
+  
+}
+onClick2(){
+  // console.log(this.value)
+  this.valueone +=2;
+  
+}
+onClick3(){
+  // console.log(this.value)
+  this.valueone +=1;
+  
+}
+
+
+onClick4(){
+  // console.log(this.value)
+  this.valuetwo +=3;
+  
+}
+onClick5(){
+  // console.log(this.value)
+  this.valuetwo +=2;
+  
+}
+onClick6(){
+  // console.log(this.value)
+  this.valuetwo +=1;
+  
+}
+
+}
